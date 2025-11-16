32cbb41b7dcfa73c835770b5708314f41530b2bb
Lucas Marta <lucas.marta0799@gmail.com>
2025-11-15 20:41:12 -0800

    Basic logging.

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,19 @@ mod user;
 
 // For some reason utoipa requires these to be imported like this for the paths to work.
 use crate::login::__path_login_request;
-//use utoipa_swagger_ui::SwaggerUi;
 use crate::login::login_request;
 use crate::register::__path_register_request;
 use crate::register::register_request;
 use crate::user::__path_user_by_id;
 use crate::user::user_by_id;
+use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::{App, HttpServer};
 use dotenvy::dotenv;
+use env_logger::Env;
 use sqlx::PgPool;
 use utoipa::OpenApi;
 use utoipa_redoc::{Redoc, Servable};
-
-// Unfortunately the OpenAPI spec only supports one contact, so I can't put William's info here.
 
 #[derive(OpenApi)]
 #[openapi(
@@ -58,6 +57,8 @@ pub struct ApiDoc;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     // Load .env file into environment.
     dotenv().expect("Failed to load `.env` file.");
 
@@ -84,12 +85,11 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(Data::new(pool.clone()))
             .service(user_by_id)
             .service(login_request)
             .service(register_request)
-            // Swagger is cool and all but Redoc looks a lot cleaner.
-            //.service(SwaggerUi::new("/docs/{_:.*}").url("/api-docs/openapi.json", ApiDoc::openapi()))
             .service(Redoc::with_url("/redoc", ApiDoc::openapi()))
     })
     .bind(server_url)?
